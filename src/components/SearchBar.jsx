211c7eb7e1883dc81833bb7dd24fe107f3ce40eb
marianojimenezperez <marianojimenezperez@gmail.com>
2023-07-16 20:40:19 -0300

    Agregado de componente SearchBar, indicador de cantidad funciona de manera estática

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+const SearchBar = () => {
+  return (
+    <div className="search__bar">
+      <div className="container">
+        <div className="search__box">
+          <h2>100</h2>
+          <form action="">
+            <input type="text" placeholder="Filter podcasts..." />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
