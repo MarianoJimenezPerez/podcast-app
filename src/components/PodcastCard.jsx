88442032d38fc0073e7c031d4f638d5a3b4b4f7e
marianojimenezperez <marianojimenezperez@gmail.com>
2023-07-16 22:50:41 -0300

    Se trabaja renderizado dinamico de podcast en la home, sin cacheo en el cliente

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -1,15 +1,13 @@
-const PodcastCard = () => {
+const PodcastCard = ({ podcast }) => {
+  const { author, image, title } = podcast;
   return (
     <div className="podcast__card">
       <div className="heading">
-        <img
-          src="https://is2-ssl.mzstatic.com/image/thumb/Podcasts126/v4/67/b3/2e/67b32e2a-2c76-062e-a137-b544eea13cf3/mza_11647040000107936939.jpg/60x60bb.png"
-          alt="Mujer"
-        />
+        <img src={image} alt="Mujer" />
       </div>
       <div className="description">
-        <h3>ALL SONGS CONSIDERED</h3>
-        <h5>Author: NPR</h5>
+        <h3>{title}</h3>
+        <h5>Author: {author}</h5>
       </div>
     </div>
   );
--- src/components/PodcastsList.jsx
+++ src/components/PodcastsList.jsx
@@ -1,14 +1,46 @@
+import { useEffect, useState } from "react";
+import { makeRequest } from "./../utils/makeRequest.js";
 import PodcastCard from "./PodcastCard";
 
 const PodcastsList = () => {
+  const [podcasts, setPodcasts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const URL = "/toppodcasts/limit=100/json";
+
+  useEffect(() => {
+    setLoading(true);
+    makeRequest
+      .get(URL)
+      .then((response) => {
+        const data = response.data;
+        const podcastsData = data.feed.entry.map((entry) => ({
+          title: entry["im:name"].label,
+          author: entry["im:artist"].label,
+          image: entry["im:image"][0].label,
+        }));
+        setPodcasts(podcastsData);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(true);
+        console.error(`Error al obtener los datos del feed: ${error.message}`);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <section className="podcasts__list">
       <div className="container">
-        <PodcastCard />
-        <PodcastCard />
-        <PodcastCard />
-        <PodcastCard />
-        <PodcastCard />
+        {loading
+          ? "Cargando..."
+          : error
+          ? "Se produjo un error al cargar los datos."
+          : podcasts.length === 0
+          ? "No se encontraron podcasts."
+          : podcasts.map((podcast, index) => (
+              <PodcastCard podcast={podcast} key={index} />
+            ))}
       </div>
     </section>
   );
