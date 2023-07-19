e79dc13671e33a88d3d5c9372e13f85bbc4862b2
marianojimenezperez <marianojimenezperez@gmail.com>
2023-07-18 21:49:20 -0300

    Se agrega usePodcastData para reducir codigo repetitivo en pages: Episode y Podcast. Se soluciona error de reproductor

--- src/pages/Episode.jsx
+++ src/pages/Episode.jsx
@@ -1,111 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { makeRequest } from "../utils/makeRequest.js";
 import Header from "../components/Header";
 import { transformHtmlToText } from "../utils/transformHtmlToText.js";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import usePodcastData from "../hooks/usePodcastData.jsx";
 
 const Episode = () => {
-  const { podcastId, episodeId } = useParams();
-  const [podcastDetails, setPodcastDetails] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [episodeFetched, setEpisodeFetched] = useState(false);
+  const { podcastDetails, loading, error } = usePodcastData();
+  const [episodeDetails, setEpisodeDetails] = useState([]);
 
-  const URL = `/lookup?id=${podcastId}`;
+  const { episodeId } = useParams();
 
-  const getPodcastImage = () => {
-    const storedPodcasts = localStorage.getItem("podcastsData");
-    if (storedPodcasts) {
-      const podcastsData = JSON.parse(storedPodcasts);
-      const found = podcastsData.find((el) => el.id === podcastId);
-      if (found) {
-        return found.image;
-      }
-    }
-    return null;
-  };
+  useEffect(() => {
+    const fetchPodcastEpisode = async () => {
+      try {
+        const response = await fetch(podcastDetails.episodes);
+        const feedData = await response.text();
 
-  const fetchPodcastDetails = () => {
-    makeRequest
-      .get(URL)
-      .then((response) => {
-        const data = response.data;
-        const podcastDetailsData = {
-          title: data.results[0].collectionName,
-          author: data.results[0].artistName,
-          description: data.results[0].collectionDescription,
-          image: getPodcastImage(),
-          episodes: data.results[0].feedUrl,
-        };
-        setPodcastDetails(podcastDetailsData);
-        setLoading(false);
-        setError(false);
-      })
-      .catch((error) => {
-        setError(true);
-        setLoading(false);
-        console.error(
-          `Error al obtener los detalles del podcast: ${error.message}`
-        );
-      });
-  };
+        if (window.DOMParser) {
+          const parser = new DOMParser();
+          const xmlDoc = parser.parseFromString(feedData, "text/xml");
 
-  const fetchPodcastEpisode = async () => {
-    try {
-      const response = await fetch(podcastDetails.episodes);
-      const feedData = await response.text();
+          const tags = xmlDoc.getElementsByTagName("item");
 
-      if (window.DOMParser) {
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(feedData, "text/xml");
+          for (let i = 0; i < tags.length; i++) {
+            const item = tags[i];
+            const guid = item.getElementsByTagName("guid")[0].textContent;
+            if (guid === episodeId) {
+              const title = item.getElementsByTagName("title")[0].textContent;
+              const description =
+                item.getElementsByTagName("description")[0].textContent;
 
-        const tags = xmlDoc.getElementsByTagName("item");
+              const enclosureTag = item.getElementsByTagName("enclosure")[0];
+              let enclosure = enclosureTag
+                ? enclosureTag.getAttribute("url")
+                : null;
 
-        const processedEpisodes = [];
-
-        for (let i = 0; i < tags.length; i++) {
-          const item = tags[i];
-          const guid = item.getElementsByTagName("guid")[0].textContent;
-          if (guid === episodeId) {
-            const title = item.getElementsByTagName("title")[0].textContent;
-            const description =
-              item.getElementsByTagName("description")[0].textContent;
-
-            const enclosureTag = item.getElementsByTagName("enclosure")[0];
-            let enclosure = enclosureTag
-              ? enclosureTag.getAttribute("url")
-              : null;
-
-            processedEpisodes.push({
-              guid: guid,
-              title: title,
-              description: transformHtmlToText(description),
-              enclosure: enclosure,
-            });
+              setEpisodeDetails((prevDetails) => [
+                ...prevDetails,
+                {
+                  guid: guid,
+                  title: title,
+                  description: transformHtmlToText(description),
+                  enclosure: enclosure,
+                },
+              ]);
+            }
           }
         }
-
-        setPodcastDetails((prevDetails) => ({
-          ...prevDetails,
-          episodes: processedEpisodes,
-        }));
-        setEpisodeFetched(true);
+      } catch (error) {
+        console.error("Error al obtener los episodios del podcast:", error);
       }
-    } catch (error) {
-      console.error("Error al obtener los episodios del podcast:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPodcastDetails();
-  }, []);
-
-  useEffect(() => {
-    if (podcastDetails && podcastDetails.episodes && !episodeFetched) {
-      fetchPodcastEpisode();
-    }
-  }, [podcastDetails, episodeFetched]);
+      console.log(episodeDetails);
+    };
+    fetchPodcastEpisode();
+  }, [episodeId, podcastDetails]);
 
   return (
     <>
@@ -139,21 +87,18 @@ const Episode = () => {
               </div>
             </div>
           ) : null}
-          {podcastDetails?.episodes ? (
+          {episodeDetails.length > 0 ? (
             <div className="episodes__section">
               <div className="shadow">
-                <h3>{podcastDetails.episodes[0].title}</h3>
-                <p>{podcastDetails.episodes[0].description}</p>
+                <h3>{episodeDetails[0].title}</h3>
+                <p>{episodeDetails[0].description}</p>
                 <audio
                   controls
                   autoPlay
                   name="media"
                   style={{ width: "100%", marginTop: "1rem" }}
                 >
-                  <source
-                    src={podcastDetails.episodes[0].enclosure}
-                    type="audio/mpeg"
-                  />
+                  <source src={episodeDetails[0].enclosure} type="audio/mpeg" />
                   Tu navegador no soporta la reproducción de audio.
                 </audio>
               </div>
